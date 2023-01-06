cda02ec3be71c62fb3aeed19b228550028a8b90d
Andrei Bartashevich <andrei.bartashevich@sitronics-kt.ru>
2023-01-06 21:47:12 +0300

    Unique color for each country

---
+++ countries.js
@@ -0,0 +1,134 @@
+// Colors are generated on https://mokole.com/palette.html.
+
+const countries = [
+  {
+    name: 'A',
+    color: '#000000', // black
+    textColor: 'white',
+  },
+  {
+    name: 'B',
+    color: '#a0522d', // sienna
+    textColor: 'white',
+  },
+  {
+    name: 'C',
+    color: '#808000', // olive
+    textColor: 'white',
+  },
+  {
+    name: 'D',
+    color: '#483d8b', // darkslateblue
+    textColor: 'white',
+  },
+  {
+    name: 'E',
+    color: '#008000', // green
+    textColor: 'white',
+  },
+  {
+    name: 'F',
+    color: '#bc8f8f', // rosybrown
+    textColor: 'black',
+  },
+  {
+    name: 'G',
+    color: '#008080', // teal
+    textColor: 'white',
+  },
+  {
+    name: 'H',
+    color: '#000080', // navy
+    textColor: 'white',
+  },
+  {
+    name: 'I',
+    color: '#9acd32', // yellowgreen
+    textColor: 'black',
+  },
+  {
+    name: 'J',
+    color: '#8b008b', // darkmagenta
+    textColor: 'white',
+  },
+  {
+    name: 'K',
+    color: '#ff4500', // orangered
+    textColor: 'white',
+  },
+  {
+    name: 'L',
+    color: '#ffa500', // orange
+    textColor: 'black',
+  },
+  {
+    name: 'M',
+    color: '#ffff00', // yellow
+    textColor: 'black',
+  },
+  {
+    name: 'N',
+    color: '#7cfc00', // lawngreen
+    textColor: 'black',
+  },
+  {
+    name: 'O',
+    color: '#8a2be2', // blueviolet
+    textColor: 'white',
+  },
+  {
+    name: 'P',
+    color: '#00ff7f', // springgreen
+    textColor: 'black',
+  },
+  {
+    name: 'Q',
+    color: '#dc143c', // crimson
+    textColor: 'white',
+  },
+  {
+    name: 'R',
+    color: '#0000ff', // blue
+    textColor: 'white',
+  },
+  {
+    name: 'S',
+    color: '#ff00ff', // fuchsia
+    textColor: 'white',
+  },
+  {
+    name: 'T',
+    color: '#f0e68c', // khaki
+    textColor: 'black',
+  },
+  {
+    name: 'U',
+    color: '#6495ed', // cornflower
+    textColor: 'black',
+  },
+  {
+    name: 'V',
+    color: '#87ceeb', // skyblue
+    textColor: 'black',
+  },
+  {
+    name: 'W',
+    color: '#ff1493', // deeppink
+    textColor: 'white',
+  },
+  {
+    name: 'X',
+    color: '#ee82ee', // violet
+    textColor: 'black',
+  },
+  {
+    name: 'Y',
+    color: '#7fffd4', // aquamarine
+    textColor: 'black',
+  },
+  {
+    name: 'Z',
+    color: '#fffff0', // ivory
+    textColor: 'black',
+  },
+]
